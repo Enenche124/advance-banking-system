70175d9c59fc632737b3e2a134529832171a6377
enenche-001 <enenchejohn@gmail.com>
2025-08-15 15:38:31 +0100

    update

--- src/main/java/com/apostle/services/CloudinaryServiceImpl.java
+++ src/main/java/com/apostle/services/CloudinaryServiceImpl.java
@@ -12,28 +12,27 @@ import java.io.IOException;
 import java.util.Map;
 
 @Service
-@RequiredArgsConstructor
 public class CloudinaryServiceImpl implements CloudinaryService{
 
 
-    private Cloudinary cloudinary;
-    @Value("${cloudinary.cloud_name}")
-    String cloudName;
-    @Value("${cloudinary.api_key}")
-    String apiKey;
-    @Value("${cloudinary.api_secret}")
-    String apiSecret;
+    private final Cloudinary cloudinary;
+//    @Value("${cloudinary.cloud_name}")
+//    String cloudName;
+//    @Value("${cloudinary.api_key}")
+//    String apiKey;
+//    @Value("${cloudinary.api_secret}")
+//    String apiSecret;
 
 
-//    public CloudinaryServiceImpl(
-//                                ,
-//                                ) {
-//        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
-//                "cloud_name", cloudName,
-//                "api_key", apiKey,
-//                "api_secret", apiSecret
-//        ));
-//    }
+    public CloudinaryServiceImpl(@Value("${cloudinary.cloud_name}") String cloudName, @Value("${cloudinary.api_key}") String apiKey,  @Value("${cloudinary.api_secret}") String apiSecret
+
+                                ) {
+        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
+        ));
+    }
 
     @Override
     public String uploadFile(MultipartFile file) {
